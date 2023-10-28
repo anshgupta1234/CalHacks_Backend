4055edf249996d8c6b32f1ca30d6dce2f9977bf6
Ansh Gupta <38015438+anshgupta1234@users.noreply.github.com>
2023-10-28 16:35:13 -0700

    Removed hume key

--- hume_calls.py
+++ hume_calls.py
@@ -4,7 +4,7 @@ from hume.models.config import ProsodyConfig
 from hume.models.config import BurstConfig
 
 def get_emotion_data(video_path):
-    client = HumeBatchClient("gQfbmvuND79DFtBCSRyYki2VPrQY89K2Ijcwxf2FMNP59nPI")
+    client = HumeBatchClient("")
     urls = []
     files = [video_path]
     configs = [FaceConfig(fps_pred=60), ProsodyConfig(granularity="conversational_turn")]
